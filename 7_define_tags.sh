1b512138d81f2fca52d953d96541ff52a7448dfb
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2023-12-15 15:53:18 +0000

    Moved the bachmann bundle settings to the case folder

--- 7_define_tags.sh
+++ 7_define_tags.sh
@@ -6,10 +6,13 @@ INPUT_heartFolder=$1
 mesh="${INPUT_heartFolder}/atrial_fibres/myocardium_fibres_l"
 BiV_folder="${INPUT_heartFolder}/surfaces_uvc/BiV"
 input_tags="./parfiles/tags_presim.json"
-BB_settings="./parfiles/bachmann_bundle_fec_settings.json"
+BB_settings="${INPUT_heartFolder}/parfiles/bachmann_bundle_fec_settings.json"
 
 LA_folder="${INPUT_heartFolder}/surfaces_uvc_LA/la/"
 RA_folder="${INPUT_heartFolder}/surfaces_uvc_RA/ra/"
+
+mkdir -p ${INPUT_heartFolder}/parfiles
+cp ./parfiles/bachmann_bundle_fec_settings.json $BB_settings
 
 CMD="python main_tags.py --heartFolder ${INPUT_heartFolder}
 						 	 --meshPath ${mesh}
