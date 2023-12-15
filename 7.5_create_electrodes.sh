733260e7f43b223645ee6746a449c0bc5e236d11
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2023-12-15 15:30:56 +0000

    Removing dependance on heartFolder

--- 7.5_create_electrodes.sh
+++ 7.5_create_electrodes.sh
@@ -2,7 +2,7 @@
 
 clear
 
-INPUT_heartFolder=$(cat /home/croderog/Desktop/IC_projects/barrows_preprocessing/parfiles/heartFolder.txt)
+INPUT_heartFolder=$1
 fascicles_settings=$INPUT_heartFolder/fascicles_settings.json
 
 CMD="python main_electrodes.py --heartFolder ${INPUT_heartFolder}
