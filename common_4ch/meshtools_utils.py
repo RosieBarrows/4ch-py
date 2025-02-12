82df9cfc26b4f0576f4e950bc7934bedc0e14ed7
Jose Alonso Solis-Lemus <jsolislemus@cemrg-mbp.home>
2025-02-12 17:49:37 +0000

    wfh_intend_fix

--- common_4ch/meshtools_utils.py
+++ common_4ch/meshtools_utils.py
@@ -146,8 +146,8 @@ def meshtool_extract_surfaces(meshname,
 	def extract_surface(surf, tag_init, tag_end, conn=":"):
 		""" Creates meshtool commands from function inputs """
 		cmd = f"meshtool extract surface -msh={meshname} -surf={output_folder}/tmp/{surf} -op={tag_init}{conn}{tag_end} -ofmt=vtk"
-       		print(f'\n{cmd}\n) 
-        	return cmd 
+		print(f'\n{cmd}\n')
+		return cmd 
 
 	tag_la_string = get_tags_from_setup(tags_setup,["LA"])
 	tag_ra_string = get_tags_from_setup(tags_setup,["RA"])
